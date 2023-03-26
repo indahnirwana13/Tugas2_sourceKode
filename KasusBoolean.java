4103827740d2b85682537e96ddb9e800bfd79f85
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-26 07:53:50 -0700

    Create KasusBoolean.java

---
+++ KasusBoolean.java
@@ -0,0 +1,29 @@
+/* NIM      : 13020210012
+Nama        : Indah Nirwana
+Hari/Tgl    : Jumat, 24 Maret 2023
+Waktu       : 16.25  PM */
+package tgs6;
+
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        boolean bool;
+        /* Algoritma */
+        bool = true;
+        if(bool) {
+            System.out.print("true\n");
+        } else
+            System.out.print("false\n");
+        if(!bool) {
+            System.out.print("salah\n");
+        } else
+            System.out.print("benar\n");
+
+    } 
+}
